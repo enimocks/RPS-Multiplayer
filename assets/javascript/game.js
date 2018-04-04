f6394c8fa4dbb11561da73f8801a4ba54c2cbaaa
Edward Nimocks <enimocks@gmail.com>
2018-04-04 14:06:31 -0400

    set up HTML with Bootstrap

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyBjV2zPqqaNybxKX4aSkN0WcC9oCgo7m_8",
+  authDomain: "train-scheduler-4905a.firebaseapp.com",
+  databaseURL: "https://train-scheduler-4905a.firebaseio.com",
+  projectId: "train-scheduler-4905a",
+  storageBucket: "",
+  messagingSenderId: "164021261126"
+};
+
+firebase.initializeApp(config);
