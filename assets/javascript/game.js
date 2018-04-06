89234f760f32e16d37dc0b0ca15130ee9b6d3007
Edward Nimocks <enimocks@gmail.com>
2018-04-06 12:50:49 -0400

    basic requirements finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,103 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBjV2zPqqaNybxKX4aSkN0WcC9oCgo7m_8",
-  authDomain: "train-scheduler-4905a.firebaseapp.com",
-  databaseURL: "https://train-scheduler-4905a.firebaseio.com",
-  projectId: "train-scheduler-4905a",
-  storageBucket: "",
-  messagingSenderId: "164021261126"
-};
+$(document).ready(function () {
 
-firebase.initializeApp(config);
+  // Initialize Firebase
+  // Database website: https://console.firebase.google.com/project/train-scheduler-4905a/database/train-scheduler-4905a/data/
+  var config = {
+    apiKey: "AIzaSyBjV2zPqqaNybxKX4aSkN0WcC9oCgo7m_8",
+    authDomain: "train-scheduler-4905a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4905a.firebaseio.com",
+    projectId: "train-scheduler-4905a",
+    storageBucket: "",
+    messagingSenderId: "164021261126"
+  };
 
-// Create a variable to reference the database
-var database = firebase.database();
+  firebase.initializeApp(config);
+
+  // Create a variable to reference the database
+  var database = firebase.database();
+
+  // Click event that pushes added trains to Firebase when the submit button is clicked, push creates new records each time a submission is entered
+  $(document).on("click", "#submit", function (event) {
+
+    event.preventDefault();
 
 
-$(document).on("click", "#submit", function (event) {
+    var name = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrainTime = $("#train-time").val().trim();
+    var frequency = $("#frequency").val().trim();
 
-  event.preventDefault();
-
-
-  var name = $("#train-name").val().trim();
-  var destination = $("#destination").val().trim();
-  var firstTrainTime = $("#train-time").val().trim();
-  var frequency = $("#frequency").val().trim();
-
-  database.ref().push({
-    name: name,
-    destination: destination,
-    firstTrainTime: firstTrainTime,
-    frequency: frequency,
-    timeAdded: firebase.database.ServerValue.TIMESTAMP
+    database.ref().push({
+      name: name,
+      destination: destination,
+      firstTrainTime: firstTrainTime,
+      frequency: frequency,
+      timeAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+    // Resets form inputs to blank after the submit button is clicked
+    $("input").val("");
   });
 
-  $("input").val("");
-});
+  database.ref().on("child_added", function (childSnapshot) {
 
-database.ref().on("child_added", function(childSnapshot) {
+    var name = childSnapshot.val().name;
+    var destination = childSnapshot.val().destination;
+    var firstTrainTime = childSnapshot.val().firstTrainTime;
+    var frequency = childSnapshot.val().frequency;
 
-  var name = childSnapshot.val().name;
-  var destination = childSnapshot.val().destination;
-  var firstTrainTime = childSnapshot.val().firstTrainTime;
-  var frequency = childSnapshot.val().frequency;
+    console.log(name);
+    console.log(destination);
+    console.log(firstTrainTime);
+    console.log(frequency);
 
-  console.log(name);
-  console.log(destination);
-  console.log(firstTrainTime);
-  console.log(frequency);
+    // TIME CALCULATIONS
+    // =========================================================================================
 
-  /* Appending a new row of table data - using ES6 syntax (templating) - when there is a child added to the root of our firebase database */
-  $(".table").append(
-    `
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(childSnapshot.val().firstTrainTime, "HH:mm").subtract(1, "years");
+    console.log("first time converted: " + firstTimeConverted);
+
+    // Current time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+    // Difference between current time and first train time (converted)
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+
+    // Minutes until next train
+    var minsTillNextTrain = frequency - tRemainder;
+    console.log("MINUTES TILL NEXT: " + minsTillNextTrain);
+
+    // Next train time
+    var nextTrainTime = moment().add(minsTillNextTrain, "minutes")
+    console.log("ARRIVAL TIME: " + moment(nextTrainTime).format("HH:mm"));
+    
+    // Formatted nextTrainTime to AM/PM time format
+    var nextArrival = moment(nextTrainTime).format("HH:mm");
+
+    // Appending a new row of table data - using ES6 syntax (templating) - when there is a child added to the root of our firebase database
+    $(".table").append(
+      `
     <tr>
       <td>${name}</td>
       <td>${destination}</td>
       <td>${frequency}</td>
-      <td></td>
-      <td></td>
+      <td>${nextArrival}</td>
+      <td>${minsTillNextTrain}</td>
     </tr>
     `
-  )
-  
+    )
+
+    // Handle any errors
+  }, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+
+  });
+
 });
